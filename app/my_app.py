e16cab279ec07354554c25cf6da558083c786ee2
Javier Valenzani <67742997+jvalenzani-hbtn@users.noreply.github.com>
2023-08-22 15:44:11 -0300

    Update my_app.py

--- app/my_app.py
+++ app/my_app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
+    name = os.environ.get("NAME", "Javiercito")
     return f"Hello {name}!"
 
 
